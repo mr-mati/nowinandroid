afd7a00a6660de6a3f1e2d07e1f72a86dfcb979d
Mohammadmehdi.moh <Mahdi60mm60@gmail.com>
2026-08-31 18:42:36 +0330

    build: update build configurations and project settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation(projects.feature.topic.impl)
     implementation(projects.feature.search.api)
     implementation(projects.feature.search.impl)
+    implementation(projects.feature.news.api)
+    implementation(projects.feature.news.impl)
     implementation(projects.feature.settings.impl)
 
     implementation(projects.core.common)
